f14ef29c320073b5c36f0f28fa5ac4f7e36a6cef
hazem <hazemjh10@gmail.com>
2023-10-25 17:10:19 +0200

    update

--- my/src/app.js
+++ my/src/app.js
@@ -1,65 +1,31 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col, Button } from 'react-bootstrap';
-
-function App() {
-  // Create React elements without JSX
-  const container = React.createElement(Container, null,
-    React.createElement(Row, null,
-      React.createElement(Col, null,
-        React.createElement('h1', null, 'Welcome to Your React App'),
-        React.createElement('p', null, 'This is a sample React app using react-bootstrap.'),
-        React.createElement(Button, { variant: 'primary' }, 'Click me')
-      )
-    )
-  );
-
-  return container;
-}
-function App() {
-    return (
-      <Container>
-        <Row>
-          <Col>
-            <h1>Welcome to Your React App</h1>
-            <p>This is a sample React app using react-bootstrap.</p>
-            <Button variant="primary">Click me</Button>
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
-  function App() {
-    return (
-      <>
-        {/* React Fragment */}
-        <div className="App">
-          <Container>
-            <Row>
-              <Col>
-                <h1>Welcome to Your React App</h1>
-                <p>This is a sample React app using react-bootstrap.</p>
-                <Button variant="primary">Click me</Button>
-              </Col>
-            </Row>
-          </Container>
-        </div>
-      </>
-    );
-  }
+import Card from 'react-bootstrap/Card';
+import Nav from 'react-bootstrap/Nav';
   function App() {
     return (
       <>
         <div className="App">
-          <Navbar bg="dark" variant="dark">
-            <Navbar.Brand>React-Bootstrap App</Navbar.Brand>
-            <Nav className="mr-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#about">About</Nav.Link>
-              <Nav.Link href="#contact">Contact</Nav.Link>
-            </Nav>
-          </Navbar>
-  
+        <Nav
+      activeKey="/home"
+      onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+    >
+      <Nav.Item>
+        <Nav.Link href="/home">Active</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="link-1">Link</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="link-2">Link</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="disabled" disabled>
+          Disabled
+        </Nav.Link>
+      </Nav.Item>
+    </Nav>
           <Container>
             <Row>
               <Col>
